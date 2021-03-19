c9deaa81cf772e0fb64500e6f0ca41ba839f08fb
Hugo Jhojan Lora Saravia <hugolora2014@gmail.com>
2021-03-18 23:21:58 -0400

    modelos, controladores y migraciones base

---
+++ app/Models/bus_chofer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class bus_chofer extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2021_03_19_032017_create_tramos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTramosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tramos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tramos');
+    }
+}
