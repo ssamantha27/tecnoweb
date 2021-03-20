e87d7d3438eecbc70046c6089e3aeae886d2ab4c
Hugo Jhojan Lora Saravia <hugolora2014@gmail.com>
2021-03-20 15:34:20 -0400

    Simple login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -73,7 +75,7 @@ class LoginController extends Controller
         $this->validateLogin($request);
 
         if (Auth::attempt(['email' => $request->email,'password' => $request->password])){
-            return redirect()->route('main');
+            return redirect()->route('home');
         }
 
         return back()
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RolController;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +16,7 @@ use App\Http\Controllers\RolController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[LoginController::class, 'showLoginForm']);
 
 Auth::routes();
 
