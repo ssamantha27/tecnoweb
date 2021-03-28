306e908fb5ca5cc51c85c5e91da5e11df9f298ca
ssamantha27 <81169375+ssamantha27@users.noreply.github.com>
2021-03-28 00:35:09 -0300

    Update BusController.php

--- app/Http/Controllers/BusController.php
+++ app/Http/Controllers/BusController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class BusController extends Controller
 {
-    //
+     public function store(Request $request)
+    {
+        
+        $bus = new Bus();
+        $bus->nombre = $request->nombre;
+        $bus->descripcion = $request->descripcion;
+        $bus->modelo = $request->modelo;
+        $bus->capacidad = $request->capacidad;
+        $bus->estado = $request->estado;
+        $bus->id = $id->id;
+        $bus->id_ruta = $request->id_ruta;
+        $bus->save();
+        
+    
+    }
 }
