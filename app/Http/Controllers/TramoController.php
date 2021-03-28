9e171fd471c2b16aae411484bedd0488e9b91aa4
ssamantha27 <81169375+ssamantha27@users.noreply.github.com>
2021-03-28 00:37:06 -0300

    Update TramoController.php

--- app/Http/Controllers/TramoController.php
+++ app/Http/Controllers/TramoController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class TramoController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        
+        $tramo = new Tramo();
+        $tramo->nombre = $request->nombre;
+        $tramo->descripcion = $request->descripcion;
+        $tramo->id = $id->id;
+        $tramo->id_ruta = $request->id_ruta;
+        $tramo->save();
+    
+    }
 }
