aabaa4d3c3ea0f2d8f68cf0415a1dd23cb40a27c
ssamantha27 <81169375+ssamantha27@users.noreply.github.com>
2021-03-28 00:36:14 -0300

    Update RutaController.php

--- app/Http/Controllers/RutaController.php
+++ app/Http/Controllers/RutaController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class RutaController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        
+        $ruta = new Ruta();
+        $ruta->nombre = $request->nombre;
+        $ruta->descripcion = $request->descripcion;
+        $ruta->lat_ini = $request->lat_ini;
+        $ruta->genero = $request->genero;
+        $ruta->long_ini = $request->long_ini;
+        $ruta->lat_fin = $request->lat_fin;
+        $ruta->long_fin = $request->long_fin;
+        $ruta->id = $id->id;
+        $ruta->save();
+    
+    }
 }
